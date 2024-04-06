78660ec9a4b7b5b694eff44e6789f73b7959f307
WolaDev <bozzbhoi@gmail.com>
2024-04-06 22:56:32 +0100

    DBContext Content Edited

--- DAL/SharpCutDbContext.cs
+++ DAL/SharpCutDbContext.cs
@@ -6,8 +6,8 @@ namespace Desktop;
 public class SharpCutDbContext(DbContextOptions<SharpCutDbContext> options) : DbContext(options)
 {
     public DbSet<ClientModel> Clients  => Set<ClientModel>();
-        public DbSet<ClientModel> Barbers  => Set<ClientModel>();
-    public DbSet<ClientModel> Appointments  => Set<ClientModel>();
+        public DbSet<ClientModel> Barbers  => Set<BarbersModel>();
+    public DbSet<ClientModel> Appointments  => Set<AppointmentsModel>();
     public DbSet<ServiceModel> Services  => Set<ServiceModel>();
 
 
